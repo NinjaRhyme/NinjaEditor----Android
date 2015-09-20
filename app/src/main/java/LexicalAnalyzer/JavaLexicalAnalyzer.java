655c912256d96b631af2a0016e557bc657f2a4c3
NinjaRhyme <NinjaRhyme@gmail.com>
2015-09-20 14:43:35 +0800

    Update lexical analyzer

--- app/src/main/java/LexicalAnalyzer/JavaLexicalAnalyzer.java
+++ app/src/main/java/LexicalAnalyzer/JavaLexicalAnalyzer.java
@@ -1,8 +1,11 @@
 package LexicalAnalyzer;
 
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.text.Editable;
 import android.text.Spanned;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ForegroundColorSpan;
 import android.text.style.StyleSpan;
 
 public class JavaLexicalAnalyzer {
@@ -32,7 +35,8 @@ public class JavaLexicalAnalyzer {
     }
 
     //----------------------------------------------------------------------------------------------------
-    Editable m_editable;
+    private static String s_punctuations = "+-*/()[]{}!@#$%^&<>=|:;?~.,\'`\"";
+    private Editable m_editable;
     private String m_data;
     private int m_len;
     private int m_pos;
@@ -53,7 +57,7 @@ public class JavaLexicalAnalyzer {
                 handleNumber();
             } else if (isLetterOrUnderline(m_data.charAt(m_pos))) {
                 handleKeywordOrIdentifier();
-            } else {
+            } else if (isPunctuation(m_data.charAt(m_pos))) {
                 handlePunctuation();
             }
         }
@@ -74,32 +78,203 @@ public class JavaLexicalAnalyzer {
     }
 
     private boolean handleNumber() {
+        int begin = m_pos;
 
+        // Octal(begins with "0") or Hexadecimal(begins with "0x" or "0X") or float or double number
+        if (m_data.charAt(m_pos) == '0') {
+            if (m_data.charAt(m_pos + 1)  == 'x' || m_data.charAt(m_pos + 1)  == 'X') {
+                m_pos += 2;
+                while (isHexadecimalNumber(m_data.charAt(m_pos))) {
+                    ++m_pos;
+                }
+            } else if (!isDot(m_data.charAt(m_pos + 1))) {
+                ++m_pos;
+                while (isOctalNumber(m_data.charAt(m_pos))) {
+                    ++m_pos;
+                }
+            } else {
+                // Float or Double number
+                m_pos += 2;
+                while (isDecimalNumber(m_data.charAt(m_pos))) {
+                    ++m_pos;
+                }
+                if (m_data.charAt(m_pos) == 'f' || m_data.charAt(m_pos) == 'F') {
+                    ++m_pos;
+                }
+            }
+        } else {
+            // Decimal number
+            while (isDecimalNumber(m_data.charAt(m_pos))) {
+                ++m_pos;
+            }
 
+            // Float or Double number
+            if (isDot(m_data.charAt(m_pos))) {
+                ++m_pos;
+                while (isDecimalNumber(m_data.charAt(m_pos))) {
+                    ++m_pos;
+                }
+                if (m_data.charAt(m_pos) == 'f' || m_data.charAt(m_pos) == 'F') {
+                    ++m_pos;
+                }
+            }
+        }
+
+        renderNumber(begin, m_pos);
         return true;
     }
 
     private boolean handleKeywordOrIdentifier() {
         int begin = m_pos;
+
+        // Get word
         while (isLetterOrUnderline(m_data.charAt(m_pos)))  {
             ++m_pos;
         }
         while (isLetterOrNumberOrUnderline(m_data.charAt(m_pos))) {
             ++m_pos;
         }
+
+        // Match word
         String word = m_data.substring(begin, m_pos);
-        for(TokenEnum token : TokenEnum.values()) {
+        for (TokenEnum token : TokenEnum.values()) {
             if (word.equals(token.toString())) {
+                // Keyword
                 renderKeyword(begin, m_pos);
                 return true;
             }
         }
 
+        // Identifier
+        renderIdentifier(begin, m_pos);
         return true;
     }
 
     private boolean handlePunctuation() {
+        int begin = m_pos;
+        ++m_pos;
 
+        switch (m_data.charAt(m_pos - 1)) {
+            case '+':
+                // "++" "+="
+                if (m_data.charAt(m_pos) == '+' || m_data.charAt(m_pos) == '=') {
+                    ++m_pos;
+                }
+                break;
+
+            case '-':
+                //"--" "-="
+                if (m_data.charAt(m_pos) == '-' || m_data.charAt(m_pos) == '=') {
+                    ++m_pos;
+                }
+                break;
+
+            case '*':
+                // "*="
+                if (m_data.charAt(m_pos) == '=') {
+                    ++m_pos;
+                }
+                break;
+
+            case '/':
+                // "/="
+                if (m_data.charAt(m_pos) == '=') {
+                    ++m_pos;
+                }
+                break;
+
+            case '!':
+                // "!="
+                if (m_data.charAt(m_pos) == '=') {
+                    ++m_pos;
+                }
+                break;
+
+            case '@':
+                break;
+
+            case '&':
+                // "&&" "&="
+                if (m_data.charAt(m_pos) == '&' || m_data.charAt(m_pos) == '=') {
+                    ++m_pos;
+                }
+                break;
+
+            case '<':
+                // "<=" "<<"
+                if (m_data.charAt(m_pos) == '=' || m_data.charAt(m_pos) == '<') {
+                    ++m_pos;
+                }
+                break;
+
+            case '>':
+                // ">=" ">>"
+                if (m_data.charAt(m_pos) == '=' || m_data.charAt(m_pos) == '>') {
+                    ++m_pos;
+                }
+                break;
+
+            case '=':
+                //"=="
+                if (m_data.charAt(m_pos) == '=') {
+                    ++m_pos;
+                }
+                break;
+
+            case '|':
+                // "||" "|="
+                if (m_data.charAt(m_pos) == '|' || m_data.charAt(m_pos) == '=') {
+                    ++m_pos;
+                }
+                break;
+
+            case ':':
+                break;
+
+            case '\'':
+                while (m_data.charAt(m_pos) != '\'' && m_data.charAt(m_pos) != '\0') {
+                    if (m_data.charAt(m_pos) == '\\') {
+                        ++m_pos;
+                        if (m_data.charAt(m_pos) == '\'' || m_data.charAt(m_pos) == '\\') {
+                            ++m_pos;
+                        }
+                    } else {
+                        ++m_pos;
+                    }
+                }
+                if (m_data.charAt(m_pos) == '\'') {
+                    ++m_pos;
+                }
+
+                // Literal
+                renderLiteral(begin, m_pos);
+                return true;
+
+            case '"':
+                while (m_data.charAt(m_pos) != '"' && m_data.charAt(m_pos) != '\0') {
+                    if (m_data.charAt(m_pos) == '\\') {
+                        ++m_pos;
+                        if (m_data.charAt(m_pos) == '"' || m_data.charAt(m_pos) == '\\') {
+                            ++m_pos;
+                        }
+                    } else {
+                        ++m_pos;
+                    }
+                }
+                if (m_data.charAt(m_pos) == '"') {
+                    ++m_pos;
+                }
+
+                // Literal
+                renderLiteral(begin, m_pos);
+                return true;
+
+            default:
+                break;
+        }
+
+        // Punctuation
+        renderPunctuation(begin, m_pos);
         return true;
     }
 
@@ -107,6 +282,10 @@ public class JavaLexicalAnalyzer {
     //----------------------------------------------------------------------------------------------------
     private boolean isDecimalNumber(char input) {
         return '0' <= input && input <= '9';
+    }
+
+    private boolean isOctalNumber(char input) {
+        return '0' <= input && input <= '7';
     }
 
     private boolean isHexadecimalNumber(char input) {
@@ -129,6 +308,15 @@ public class JavaLexicalAnalyzer {
         return input == ' ' || input == '\t' || input == '\n' || input == '\r';
     }
 
+    private boolean isPunctuation(char input) {
+        for (int i = 0; i < s_punctuations.length(); ++i) {
+            if (s_punctuations.charAt(i) == input) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     private boolean isEnd() {
         return m_len <= m_pos;
     }
@@ -136,10 +324,22 @@ public class JavaLexicalAnalyzer {
     // Render
     //----------------------------------------------------------------------------------------------------
     private void renderNumber(int begin, int end) {
-        m_editable.setSpan(new StyleSpan(Typeface.BOLD), begin, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        m_editable.setSpan(new ForegroundColorSpan(Color.BLUE), begin, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
     }
 
     private void renderKeyword(int begin, int end) {
-        m_editable.setSpan(new StyleSpan(Typeface.BOLD), begin, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        m_editable.setSpan(new ForegroundColorSpan(Color.MAGENTA), begin, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+    }
+
+    private void renderIdentifier(int begin, int end) {
+        m_editable.setSpan(new ForegroundColorSpan(Color.CYAN), begin, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+    }
+
+    private void renderLiteral(int begin, int end) {
+        m_editable.setSpan(new ForegroundColorSpan(Color.BLACK), begin, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+    }
+
+    private void renderPunctuation(int begin, int end) {
+        m_editable.setSpan(new ForegroundColorSpan(Color.DKGRAY), begin, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
     }
 }
